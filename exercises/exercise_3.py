a56b965672eb61ad8de870589e1fa01f1494690f
ethan <ethan.yu@has.hc.edu.tw>
2023-09-26 13:06:01 +0800

    ex3 done

--- exercises/exercise_3.py
+++ exercises/exercise_3.py
@@ -1,7 +1,8 @@
 def print_characters():
     # Your task is to use a for loop to iterate over the characters in the string "Python" and print each character.
-    pass
-
+    text ="Python"
+    for character in text:
+        print(character)
 def main():
     print_characters()
 
